c46638d865ecd57ca726f9f011001250e4ad31e0
Fikasentani <mpilo.dlamini@yahoo.com>
2019-10-07 13:34:29 +0200

    added htlm css and a XHTTPrequest that is not working

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-alert('hi from app js');
+document.addEventListener("DOMCotentLoaded", function(event) {
+    let request = new XMLHttpRequest();
+    request.open('GET', '/api/todos', true);
+
+    request.onload = function() {
+        if (this.status == 200 && this.status < 400) {
+            //success
+            var data = JSON.parse(this.response);
+            console.log("sucessul call");
+        } else {
+
+        }
+    };
+    request.onerror = function() {
+        console.log('could not connect to database');
+    };
+    request.send();
+});
