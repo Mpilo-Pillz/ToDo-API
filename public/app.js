3cd6030fe1078f3ea16bc74d990801851ff9b3b1
Fikasentani <mpilo.dlamini@yahoo.com>
2019-10-17 06:59:11 +0200

    added DOMcontent loaded so that api call is made when the page loads instead of a click,taking the todos retunred from api displaying them to ui. Thee is a bug only the first item gets the done class added to it not based on condition

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("click", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     var request = new XMLHttpRequest();
     request.open('GET', '/api/todos', true);
 
@@ -8,6 +8,7 @@ document.addEventListener("click", function(event) {
             var data = JSON.parse(this.response);
             console.log("sucessul call");
             console.log(data);
+            addTodos(data);
         } else {
 
         }
@@ -17,6 +18,21 @@ document.addEventListener("click", function(event) {
     };
     request.send();
 });
+
+function addTodos(todos) {
+    //add todos to page here by looping through each and taking the name and appending it
+    todos.forEach(function(todo){
+        console.log(todo.name);
+        // var newTodo = '<li class="task">'+todo.name + todo.completed +'</li>';
+        var newTodo = `<li class="task"><em>${todo.name}</em> completed status is <strong>${todo.completed}</strong></li>`;
+        
+        if(todo.completed === true) {
+            document.querySelector('.task').classList.add("done");     
+        } 
+        document.querySelector('.list').insertAdjacentHTML('beforeend', newTodo);
+        
+    })
+}
 
 // document.addEventListener("click", function(event) {
 //     var request = new XMLHttpRequest();
